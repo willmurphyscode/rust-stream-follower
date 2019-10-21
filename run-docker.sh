b09dea59781c102b9bdba8872506c13afdfb6a3c
Will Murphy <willmurphyscode@gmail.com>
2019-10-21 07:58:44 -0700

    Attach to docker shell
    I kept accidentally backgrounding the container instead of killing it
    and then getting errors when I tried to start the next client.

--- run-docker.sh
+++ run-docker.sh
@@ -4,7 +4,7 @@ set -u
 
 docker build . -t streamy
 
-docker run -t --env API_KEY="$API_KEY" --env API_SECRET="$API_SECRET" \
+docker run -ti --env API_KEY="$API_KEY" --env API_SECRET="$API_SECRET" \
     --env ACCESS_TOKEN="$ACCESS_TOKEN" \
     --env ACCESS_SECRET="$ACCESS_SECRET" \
     --env PORT="$PORT" \
